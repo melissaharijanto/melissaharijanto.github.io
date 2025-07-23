ec0995ffbd3e3aec5a273a2007a94efc03204d2c
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-07-23 18:56:08 +0700

    add bold style for active links

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -1,17 +1,29 @@
-import { Link } from '@tanstack/react-router';
+import { Link, useRouterState } from '@tanstack/react-router';
 import Logo from './Logo';
 import { styles } from '../../utils/styles';
+import { useEffect, useState } from 'react';
 
 const Navigation = () => {
+    const [routerPath, setRouterPath] = useState<string>('');
+    const router = useRouterState();
+    
+    useEffect(() => {
+        if (router) {
+            setRouterPath(router.location.pathname);
+        }
+
+        console.log(routerPath)
+    })
+
     return (
         <div className="hidden md:flex flex-col font-outfit h-screen py-8 min-w-[10%]">
             <Logo />
             <div className="flex flex-col justify-between h-full">
                 <div className={`flex flex-col`}>
-                    <Link to="/" className={styles.navLink}>About</Link>
-                    <Link to="/works" className={styles.navLink}>Works</Link>
-                    <Link to="/experience" className={styles.navLink}>Experience</Link>
-                    <Link to="/playground" className={styles.navLink}>Playground</Link>
+                    <Link to="/" className={`${styles.navLink} ${routerPath === "/"? "font-semibold": ""}`}>About</Link>
+                    <Link to="/works" className={`${styles.navLink} ${routerPath === "/works"? "font-semibold": ""}`}>Works</Link>
+                    <Link to="/experience" className={`${styles.navLink} ${routerPath === "/experience"? "font-semibold": ""}`}>Experience</Link>
+                    <Link to="/playground" className={`${styles.navLink} ${routerPath === "/playground"? "font-semibold": ""}`}>Playground</Link>
                 </div>
                 <div className="flex flex-col">
                     <p className="text-black">Links</p>
