964242ed258d9df64ad876d49dd3a7c7c233a0b5
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-07-24 20:30:49 +0700

    add new projects

--- src/assets/components/ProjectComponent.tsx
+++ src/assets/components/ProjectComponent.tsx
@@ -4,7 +4,7 @@ import type { ProjectComponentProps } from '../../utils/types/ProjectComponentPr
 const ProjectComponent: React.FC<ProjectComponentProps> = ({projectProps, key} : ProjectComponentProps) => {
     return (
         <div id={key} className='border-1 border-black/25 flex-col flex gap-y-4 bg-white/40 rounded-xl hover:scale-95 transition-all ease-in cursor-pointer'>
-            <img src={projectProps.image} alt={projectProps.title} className="w-full h-auto rounded-lg shadow-lg" />
+            <img src={projectProps.image} alt={projectProps.title} className="w-full h-auto rounded-xl shadow-lg" />
         </div>
     );
 };
--- src/utils/data/projects.ts
+++ src/utils/data/projects.ts
@@ -20,13 +20,40 @@ export const projects: ProjectProps[] = [
         tags: ["Virtual Reality", "Unity 3D", "Game Development"]
     },
     {
+        title: "Classroom 360",
+        description: "",
+        image: "/classroom360.jpg",
+        link: "https://github.com/melissaharijanto/pink-unicorn",
+        startDate: new Date("May 2023"),
+        endDate: new Date("June 2023"),
+        tags: ["React 360", "Web Development"]
+    },
+    {
+        title: "GameRoom",
+        description: "",
+        image: "/gameroom.jpg",
+        link: "https://github.com/melissaharijanto/gameroom-frontend",
+        startDate: new Date("December 2022"),
+        endDate: new Date("January 2023"),
+        tags: ["Inactive", "Prototyping", "Full-Stack Development"]
+    },
+    {
         title: "PINUS Study",
         description: "",
         image: "/pinus-study.jpg",
         link: "https://pinusstudy.com",
         startDate: new Date("August 2022"),
-        endDate: new Date("July 2022"),
-        tags: ["Virtual Reality", "Unity 3D", "Game Development"]
+        endDate: new Date("July 2023"),
+        tags: ["Frontend Development"]
+    },
+    {
+        title: "TrackO",
+        description: "",
+        image: "/tracko.jpg",
+        link: "https://github.com/melissaharijanto/tp",
+        startDate: new Date("September 2023"),
+        endDate: new Date("November 2023"),
+        tags: ["Application Development", "Frontend Development"]
     },
     {
         title: "Travel Log",
