b76319ab406703f739d5fe4dae6889b5287eccc8
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-07-25 12:26:57 +0700

    add transition

--- src/routes/playground.tsx
+++ src/routes/playground.tsx
@@ -1,9 +1,38 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useEffect, useRef } from 'react';
+import { useTransitionTimeline } from '../utils/store/TransitionContext';
+import gsap from 'gsap';
 
 export const Route = createFileRoute('/playground')({
     component: RouteComponent,
 });
 
 function RouteComponent() {
-    return <div>Hello "/playground"!</div>;
+    const container = useRef<HTMLDivElement>(null);
+    const tl = useTransitionTimeline();
+
+    useEffect(() => {
+        const ctx = gsap.context(() => {
+            tl.clear();
+            tl.fromTo(
+                container.current,
+                { opacity: 0, y: -40, filter: 'blur(20px)' },
+                {
+                    opacity: 1,
+                    y: 0,
+                    filter: 'blur(0px)',
+                    duration: 0.6,
+                    ease: 'power2.out',
+                }
+            );
+            tl.play();
+        }, container);
+
+        return () => ctx.revert();
+    }, []);
+    return (
+        <div ref={container} id="page-transition-container">
+            <p className="font-outfit"> coming soon :)</p>
+        </div>
+    );
 }
