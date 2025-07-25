d34fe5d4731592bc1c913a0d8e6efbf1c1725891
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-07-25 14:16:22 +0700

    Updates

--- assets/index-CXjMGdlN.js
+++ assets/index-CXjMGdlN.js
@@ -1,4 +1,4 @@
-import{r as l,g as j,j as e,s as y}from"./index-B8Cdo7wG.js";import{p as v}from"./projects-DebLRHzT.js";/*!
+import{r as l,g as j,j as e,s as y}from"./index-D92SFp9W.js";import{p as v}from"./projects-DebLRHzT.js";/*!
  * @gsap/react 2.1.2
  * https://gsap.com
  *
