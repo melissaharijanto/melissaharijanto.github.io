a721000c40bb83b2dfbd980e788e865ac43d87c7
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2025-07-25 12:55:00 +0700

    change site title and add gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [tanstackRouter({
-    target: 'react',
-    autoCodeSplitting: true,
-    }), 
-    react()],
-})
+    plugins: [
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
+        react(),
+    ],
+    build: {
+        outDir: 'dist',
+    },
+});
